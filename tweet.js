8b3fd02d981acc4be182959b90d3dda8d92d27a0
Dooski <dmckeon495@gmail.com>
2020-05-14 18:07:35 -0400

    set up for heroku

--- tweet.js
+++ tweet.js
@@ -21,7 +21,12 @@ let keys = new aws.S3({
     access_token_secret: process.env.TOKEN_SECRET
 });
 
-let T = new Twit(keys)
+let T = new Twit({
+    consumer_key: keys.consumer_key,
+    consumer_secret: keys.consumer_secret,
+    access_token: keys.access_token,
+    access_token_secret: keys.access_token_secret
+})
 
 // TWEETS
 function go() {
